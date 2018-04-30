4679058fc3b8662412639ffb6075025e6a73e38e
Ryan Sparks <ryansparksm@gmail.com>
2018-04-30 00:09:32 -0400

    They see me rollin...

---
+++ stat.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+ 
+int main(int argc, char **argv)
+{
+    if(argc != 2)    
+        return 1;
+ 
+    struct stat fileStat;
+    if(stat(argv[1],&fileStat) < 0)    
+        return 1;
+ 
+    printf("File: %s\n",argv[1]);
+    printf("---------------------------\n");
+    printf("Size: %d bytes\t\t",fileStat.st_size);
+    printf("Blocks: %d\t",fileStat.st_blocks);
+    printf("IOBlocks: %d\t\n",fileStat.st_blksize);
+
+    printf("Device: %d\t",fileStat.st_dev);
+    printf("Inode: %d\t",fileStat.st_ino);
+    printf("Links: \t%d\n",fileStat.st_nlink);
+
+    printf("Access: ");
+
+    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+    printf("\tUid: %d\t",fileStat.st_uid);
+    printf("Gid: %d\t\n",fileStat.st_gid);
+
+    printf("Access: %d\t",fileStat.st_atime);
+    printf("Modify: %d\t",fileStat.st_mtime);
+    printf("Change: %d\t\n",fileStat.st_ctime);
+    return 0;
+}
