ea4684689c8ad4ae3f3d28e920f2546a8c172d95
Ryan Sparks <ryansparksm@gmail.com>
2018-04-29 23:44:44 -0400

    How to end the world in 11 easy steps

---
+++ rmdir.c
@@ -0,0 +1,28 @@
+#include <ftw.h>
+#include <stdio.h>
+#include <stdlib.h>
+//this program can and will remove unempty directories
+static int removeDirs(const char *dirName, const struct stat *sbuf, int type, struct FTW *ftwb)
+{
+    if(remove(dirName) < 0) //catches errors with directory names
+    {
+        perror("REMOVAL ERROR");
+        return -1;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) //mainly detects errors in inputs or directories 
+{
+    if (argc != 2) //to execute, use ./a.out DIRTOREMOVE
+    {
+        printf("Did not enter directory or path\n");
+        exit(1);
+    }
+    if (nftw(argv[1], removeDirs,10, FTW_DEPTH|FTW_MOUNT|FTW_PHYS) < 0)
+    {
+        perror("NOT A VALID DIRECTORY");
+        exit(1);
+    }
+    return 0;
+}
